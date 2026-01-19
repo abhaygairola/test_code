6577d5501dccb2676ce18a8cae804fa3b4e67af5
abhaygairola <abhaygairola8@gmail.com>
2026-01-19 17:51:03 +0530

    yo

--- striversheet/src/main/java/com/learn/countdigit.java
+++ striversheet/src/main/java/com/learn/countdigit.java
@@ -6,7 +6,7 @@ public class countdigit {
         return ((int) (Math.log10(s) + 1));
 
     }
-    // public static void main(String[] args) {
+    // public static void main(String[] args) {  
 
     // Scanner s = new Scanner(System.in);
     // System.out.println("Give me the number:");
